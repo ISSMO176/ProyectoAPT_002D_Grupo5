49879caf213e5880dd61118dadb46b5c0ce7fb07
Ivan Sanhueza M. <102754077+ISSMO176@users.noreply.github.com>
2024-12-06 13:59:32 -0300

    Delete proyecto directory

--- proyecto/backend/prisma/migrations/20241113045627_add_activo_field_to_usuario/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Usuario" ADD COLUMN     "activo" BOOLEAN NOT NULL DEFAULT true,
-ALTER COLUMN "apellido_materno" DROP NOT NULL;
--- proyecto/backend/src/routes/respuestaRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { guardarRespuestas, obtenerEstadisticasRespuestas } from '../controllers/respuestaController.js';
-import authMiddleware from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-router.post('/', authMiddleware, guardarRespuestas);
-router.get('/:encuestaId/estadisticas', authMiddleware, obtenerEstadisticasRespuestas);
-export default router;
